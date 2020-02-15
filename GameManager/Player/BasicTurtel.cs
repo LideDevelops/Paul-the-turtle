e5d74d7af7670299a785e8337927dc80832237fb
Lide <LideDevelops@t-online.de>
2020-02-15 21:33:12 +0100

    Started implementation for Player class

---
+++ GameManager/Player/BasicTurtel.cs
@@ -0,0 +1,15 @@
+﻿using com.theTurtlePaul.PlayerArea.GameManager;
+using GameManager.Simulation.Scheduler;
+
+namespace GameManager
+{
+    public abstract class BasicTurtel : Player
+    {
+        public void StartTurtleMain(GameField gameField, CommanderScheduler scheduler)
+        {
+            TurtleMain();
+        }
+
+        public abstract void TurtleMain();
+    }
+}
---
+++ GameManagerTest/Player/PlayerTest.cs
@@ -0,0 +1,37 @@
+﻿using com.theTurtlePaul.PlayerArea.GameManager;
+using GameManager;
+using GameManager.Simulation.Scheduler;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GameManagerTest
+{
+    public class PlayerTest
+    {
+        private Player testee;
+        private GameField gameFieldMock;
+        private CommanderScheduler commanderSchedulerMock;
+
+        public PlayerTest()
+        {
+            gameFieldMock = Substitute.For<GameField>();
+            commanderSchedulerMock = Substitute.For<CommanderScheduler>();
+            testee = Substitute.ForPartsOf<BasicTurtel>();
+        }
+
+        [Fact]
+        public void StartTurtleMainTest()
+        {
+            testee.StartTurtleMain(gameFieldMock, commanderSchedulerMock);
+            testee.Received(1).TurtleMain();
+        }
+
+        [Fact]
+        public void MoveForwardTest()
+        {
+        }
+    }
+}
